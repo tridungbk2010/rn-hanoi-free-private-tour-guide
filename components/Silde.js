19fb478dacdb58c5f6f73108d1b75757915fa9bb
Ho Tri Dung <chris.tridungbk@gmail.com>
2017-07-12 14:47:56 +0700

    initial project

---
+++ components/Silde.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {View, Text} from "react-native";
+
+class Silde extends Component {
+  render() {
+    return (
+      <View>
+      <Text>New class</Text>
+      </View>
+    );
+  }
+}
+
+export default Silde;
